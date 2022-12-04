c7a688f2e086c786b7ab1fc4b24ecabd47d47d7e
Alejandro de Ros <alejandro.de.ros@cieep.com>
2022-12-04 12:09:08 +0100

    Initial commit

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HeaderComponent} from "./header/header.component";
 import {IonicModule} from "@ionic/angular";
+import {MenuComponent} from "./menu/menu.component";
+import {RouterLink} from "@angular/router";
+import {AppRoutingModule} from "../app-routing.module";
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports:[HeaderComponent],
+  declarations: [HeaderComponent,MenuComponent],
+  exports:[HeaderComponent,MenuComponent],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    RouterLink
+
   ]
 })
 export class ComponentsModule { }
